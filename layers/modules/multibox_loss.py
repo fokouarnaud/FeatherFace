bb4539ce22c63f7e883a60b1ae7184c9cbfc8b37
fokou <fokouarnaud@gmail.com>
2025-07-16 22:33:07 +0100

    update

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from utils.box_utils import match, log_sum_exp
-from data import cfg_mnet
-GPU = cfg_mnet['gpu_train']
 
 class MultiBoxLoss(nn.Module):
     """SSD Weighted Loss Function
@@ -58,6 +56,9 @@ class MultiBoxLoss(nn.Module):
         priors = priors
         num = loc_data.size(0)
         num_priors = (priors.size(0))
+        
+        # Detect device from input tensors
+        device = loc_data.device
 
         # match priors (default boxes) and ground truth boxes
         loc_t = torch.Tensor(num, num_priors, 4)
@@ -69,12 +70,13 @@ class MultiBoxLoss(nn.Module):
             landms = targets[idx][:, 4:14].data
             defaults = priors.data
             match(self.threshold, truths, defaults, self.variance, labels, landms, loc_t, conf_t, landm_t, idx)
-        if GPU:
-            loc_t = loc_t.cuda()
-            conf_t = conf_t.cuda()
-            landm_t = landm_t.cuda()
+        
+        # Move tensors to the same device as input
+        loc_t = loc_t.to(device)
+        conf_t = conf_t.to(device)
+        landm_t = landm_t.to(device)
 
-        zeros = torch.tensor(0).cuda()
+        zeros = torch.tensor(0, device=device)
         # landm Loss (Smooth L1)
         # Shape: [batch,num_priors,10]
         pos1 = conf_t > zeros
